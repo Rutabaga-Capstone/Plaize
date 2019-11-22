49010b947c13fe1b3d7917b149412380f3e1129f
randy <randydxp@gmail.com>
2019-11-22 14:00:11 -0600

    Added dependencies for graphql, server entrypoint, and graphql schema

--- components/SingleUser.js
+++ components/SingleUser.js
@@ -1,8 +1,8 @@
-import React from "react"
+import React from 'react'
 
 export default function SingleUser(props) {
-	const users = props.users
-	return users.map(user => {
-		user.name
-	})
+  const users = props.users
+  return users.map(user => {
+    return user.name
+  })
 }
---
+++ server/resolvers/Query.js
@@ -0,0 +1,29 @@
+async function feed(parent, args, context) {
+  const where = args.filter
+    ? {
+        OR: [{description_contains: args.filter}, {url_contains: args.filter}]
+      }
+    : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  })
+  const count = await context.prisma
+    .linksConnection({
+      where,
+      skip: args.skip
+    })
+    .aggregate()
+    .count()
+  return {
+    links,
+    count
+  }
+}
+
+module.exports = {
+  feed
+}
