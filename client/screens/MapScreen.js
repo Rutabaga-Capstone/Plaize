5d150b86420bd889a373da9bc5d62a62c0c09ad3
el-fer <fer@innovaresip.com>
2019-11-25 17:01:04 -0600

    added pins to map

--- client/screens/MapScreen.js
+++ client/screens/MapScreen.js
@@ -3,13 +3,25 @@ import {Text, View, SafeAreaView} from 'react-native'
 import Map from '../components/Map'
 import Plants from '../components/Plants'
 
-// A placeholder until we get our own location
+// A placeholder location until we get our own location and pins until we fetch them from the db
+
 const region = {
   latitude: 41.895506,
   longitude: -87.639014,
   latitudeDelta: 0.0922,
   longitudeDelta: 0.0421
 }
+
+const pins = [
+  {
+    coordinate: {latitude: 41.895506, longitude: -87.639014},
+    title: 'Fullstack Academy'
+  },
+  {
+    coordinate: {latitude: 41.896461, longitude: -87.641228},
+    title: 'Starbucks'
+  }
+]
 
 const styles = {
   container: {
@@ -26,14 +38,9 @@ export default class MapScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <SafeAreaView style={styles.container}>
-          <Map region={region} places={this.state.pins} />
-        </SafeAreaView>
-        <View>
-          <Plants />
-        </View>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Map region={region} pins={pins} />
+      </SafeAreaView>
     )
   }
 }
