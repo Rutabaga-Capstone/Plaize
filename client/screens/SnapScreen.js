ffb28ad1356588b4c95911204623255d989622fc
randy <randydxp@gmail.com>
2019-11-26 10:55:53 -0600

    Snap screen fix

--- client/screens/SnapScreen.js
+++ client/screens/SnapScreen.js
@@ -1,69 +1,71 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
-
+import React from 'react'
+import {Text, View, TouchableOpacity} from 'react-native'
+import * as Permissions from 'expo-permissions'
+import {Camera} from 'expo-camera'
 
 export default class CameraExample extends React.Component {
-    state = {
-        hasCameraPermission: null,
-        type: Camera.Constants.Type.back,
-    };
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back
+  }
 
-    async componentDidMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({ hasCameraPermission: status === 'granted' });
+  async componentDidMount() {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({hasCameraPermission: status === 'granted'})
+  }
+
+  takePicture = () => {
+    console.log(this.camera)
+    if (this.camera) {
+      this.camera.takePictureAsync({onPictureSaved: this.onPictureSaved})
     }
+  }
 
-    takePicture = () => {
-        console.log(this.camera)
-        if (this.camera) {
-            this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
-        }
-    };
+  onPictureSaved = async photo => {
+    await FileSystem.moveAsync({
+      from: photo.uri,
+      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`
+    })
+    this.setState({newPhotos: true})
+  }
 
-
-    onPictureSaved = async photo => {
-        await FileSystem.moveAsync({
-            from: photo.uri,
-            to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
-        });
-        this.setState({ newPhotos: true });
+  render() {
+    const {hasCameraPermission} = this.state
+    if (hasCameraPermission === null) {
+      return <View />
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>
+    } else {
+      return (
+        <View style={{flex: 1}}>
+          <Camera style={{flex: 1}} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row'
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center'
+                }}
+                onPress={() => {
+                  console.log(this.camera)
+                  this.takePicture()
+                }}
+              >
+                <Text style={{fontSize: 18, marginBottom: 10, color: 'white'}}>
+                  {' '}
+                  Take Picture!{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      )
     }
-
-
-    render() {
-        const { hasCameraPermission } = this.state;
-        if (hasCameraPermission === null) {
-            return <View />;
-        } else if (hasCameraPermission === false) {
-            return <Text>No access to camera</Text>;
-        } else {
-            return (
-                <View style={{ flex: 1 }}>
-                    <Camera style={{ flex: 1 }} type={this.state.type}>
-                        <View
-                            style={{
-                                flex: 1,
-                                backgroundColor: 'transparent',
-                                flexDirection: 'row',
-                            }}>
-                            <TouchableOpacity
-                                style={{
-                                    flex: 0.1,
-                                    alignSelf: 'flex-end',
-                                    alignItems: 'center',
-                                }}
-                                onPress={() => {
-                                    console.log(this.camera)
-                                    this.takePicture
-                                }}>
-                                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Take Picture! </Text>
-                            </TouchableOpacity>
-                        </View>
-                    </Camera>
-                </View>
-            );
-        }
-    }
+  }
 }
--- server/schema.js
+++ server/schema.js
@@ -13,9 +13,18 @@ const {gql} = require('apollo-server')
 
 const typeDefs = `
   type Query {
-    user(id: ID, name: String): User
+    user(id: ID, name: String, email: String): User
     location(lat: Float!, lng: Float!): Location
     plant(id: ID, commonName: String): Plant
+  }
+
+  type Mutation {
+    createPin(
+      users: User! @relation(name: "CREATED", direction: "IN")
+      plants: [Plant!]!
+      location: Location! @cypher (statement: "CREATE (p:Pin { lat: location.lat, long: location.long point: point({latitude: location.lat, longitude: location.long}) MATCH (n {name: user.name}) CREATE (n)-[r:CREATED {dateCreated: date()}]->(p)")
+
+    )
   }
 
   type Location {
@@ -25,24 +34,33 @@ const typeDefs = `
   }
 
   type Plant {
-    id: ID!
+    id: ID! @unique
     commonName: String
     scientificName: String!
     imageURL: String!
     description: String!
     poisonous: Boolean!
+    user: User @relation(name: "FOUND", switch (expression) {
+      case expression:
+
+        break;
+      default:
+
+    })
   }
 
   type User {
-    id: ID!
+    id: ID! @unique
     name: String!
+    email: String!
+    password: String!
     plants: [Plant!]! @relation(name: "FOUND", direction: "OUT")
     location: Location
     pins: [Pin!] @relation(name: "CREATED", direction: "OUT")
   }
 
   type Pin {
-    id: ID!
+    id: ID! @unique
     location: Location!
     users: [User!]! @relation(name: "CREATED", direction: "IN")
     plants: [Plant!]! @relation(name: "HAS_PLANTS", direction: "OUT")
@@ -56,9 +74,7 @@ const resolvers = {
     }
   },
   Mutation: {
-    CreatePin(parent, args, ctx, info) {
-      return neo4jgraphql(parent, args, ctx, inf0)
-    }
+    CreatePin(parent, args, ctx, info) {}
   }
 }
 
