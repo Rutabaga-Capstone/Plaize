e273c8dc6b541f7a6d59d1aace7f62d7171eeded
el-fer <fer@innovaresip.com>
2019-11-25 20:27:11 -0600

    added radius circle, pin color, get user location, permissions

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {View, Text, StyleSheet, Dimensions} from 'react-native'
-import MapView, {Marker} from 'react-native-maps'
+import MapView, {Marker, Circle} from 'react-native-maps'
 
 const styles = StyleSheet.create({
   container: {
@@ -18,7 +18,13 @@ const styles = StyleSheet.create({
 export default class Map extends React.Component {
   renderMarkers() {
     return this.props.pins.map((pin, i) => (
-      <Marker key={i} title={pin.title} coordinate={pin.coordinate} />
+      <Marker
+        key={i}
+        title={pin.title}
+        coordinate={pin.coordinate}
+        pinColor={pin.isPoisonous ? 'yellow' : 'green'}
+        description={pin.description}
+      />
     ))
   }
 
@@ -26,6 +32,11 @@ export default class Map extends React.Component {
     return (
       <MapView style={styles.mapStyle} region={this.props.region}>
         {this.renderMarkers()}
+        <Circle
+          radius={500}
+          center={this.props.region}
+          fillColor={'rgba(123,239,178,.25)'}
+        />
       </MapView>
     )
   }
--- client/screens/MapScreen.js
+++ client/screens/MapScreen.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import {Text, View, SafeAreaView} from 'react-native'
+import {Platform, Text, View, SafeAreaView} from 'react-native'
 import Map from '../components/Map'
 import Plants from '../components/Plants'
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
+import Constants from 'expo-constants'
 
 // A placeholder location until we get our own location and pins until we fetch them from the db
 
@@ -15,11 +18,15 @@ const region = {
 const pins = [
   {
     coordinate: {latitude: 41.895506, longitude: -87.639014},
-    title: 'Fullstack Academy'
+    title: 'Fullstack Academy',
+    isPoisonous: true,
+    description: 'Nonpoisonous'
   },
   {
     coordinate: {latitude: 41.896461, longitude: -87.641228},
-    title: 'Starbucks'
+    title: 'Starbucks',
+    isPoisonous: false,
+    description: 'Poisonous'
   }
 ]
 
@@ -33,13 +40,51 @@ const styles = {
 export default class MapScreen extends React.Component {
   state = {
     region: null,
-    pins: []
+    pins: [],
+    location: null,
+    errorMessage: null
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+      })
+    } else {
+      this._getLocationAsync()
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let {status} = await Permissions.askAsync(Permissions.LOCATION)
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied'
+      })
+    }
+
+    let location = await Location.getCurrentPositionAsync({})
+    console.log('location: ', location)
+    this.setState({location})
   }
 
   render() {
+    let text = 'Waiting..'
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location)
+    }
+
     return (
       <SafeAreaView style={styles.container}>
-        <Map region={region} pins={pins} />
+        <Map
+          region={region}
+          pins={pins}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+        />
       </SafeAreaView>
     )
   }
