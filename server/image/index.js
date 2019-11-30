cb6556d8b8e85753cfce84d41d289b423c27fecf
Pawel Bala <pawel3ala@gmail.com>
2019-11-29 20:24:57 -0600

    photo being uploaded

--- server/image/index.js
+++ server/image/index.js
@@ -10,7 +10,10 @@ const predictionServiceClient = new PredictionServiceClient({
 })
 const fs = require('fs')
 const multer = require('multer')
-const upload = multer({dest: './server/uploads/'})
+const upload = multer({
+  dest: './server/uploads/',
+  limits: {fieldSize: 25 * 1024 * 1024}
+})
 module.exports = router
 
 async function predict(filePath) {
@@ -43,28 +46,38 @@ async function predict(filePath) {
   return returnedPayload
 }
 
-router.post('/', upload.single('formKeyName'), async (req, res, next) => {
-  if (req.file) {
-    try {
-      const response = {}
-      const filePath = path.join(__dirname, '../..', req.file.path)
-      let prediction = await predict(filePath)
-      response.commonName = prediction.displayName
-        .split(' ')
-        .map(el => {
-          return el[0].toUpperCase() + el.slice(1)
+router.post(
+  '/',
+  () => {
+    console.log('HIT1')
+  },
+  upload.single('formKeyName'),
+  async (req, res, next) => {
+    console.log('HIT2')
+    console.log(req.file)
+    if (req.file) {
+      console.log('HI3')
+      try {
+        const response = {}
+        const filePath = path.join(__dirname, '../..', req.file.path)
+        let prediction = await predict(filePath)
+        response.commonName = prediction.displayName
+          .split(' ')
+          .map(el => {
+            return el[0].toUpperCase() + el.slice(1)
+          })
+          .join(' ')
+        response.score = prediction.classification.score
+        fs.unlink(filePath, err => {
+          if (err) {
+            console.err(err)
+            throw new Error('Could not delete file')
+          }
         })
-        .join(' ')
-      response.score = prediction.classification.score
-      fs.unlink(filePath, err => {
-        if (err) {
-          console.err(err)
-          throw new Error('Could not delete file')
-        }
-      })
-      res.json(response)
-    } catch (e) {
-      next(e)
+        res.json(response)
+      } catch (e) {
+        next(e)
+      }
     }
   }
-})
+)
--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ const createApp = server => {
   })
 
   // error handling endware
-  app.use((req, res, next, err) => {
+  app.use((err, req, res, next) => {
     console.error(err)
     console.error(err.stack)
     res.status(err.status || 500).send(err.message || 'Internal server error.')
