6c447d6d04a93c012dfa7065f65c255bb3137370
p3 <38990338+pawel3ala@users.noreply.github.com>
2019-12-06 16:54:44 -0600

    same commit as before but without husky plugin

--- client/screens/SnapScreen.js
+++ client/screens/SnapScreen.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react'
-import {Text, View, TouchableOpacity, StyleSheet} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 import * as Permissions from 'expo-permissions'
-import {Camera} from 'expo-camera'
+import { Camera } from 'expo-camera'
 import * as FileSystem from 'expo-file-system'
 import axios from 'axios'
-import {Ionicons, SimpleLineIcons} from '@expo/vector-icons'
+import { Ionicons, SimpleLineIcons } from '@expo/vector-icons'
 import PlantModal from '../components/PlantModal'
-import {useMutation, useApolloClient} from '@apollo/react-hooks'
+import { useMutation, useApolloClient } from '@apollo/react-hooks'
 import {
   CREATE_PIN_PLANT,
   ADD_PIN_PLANT_TO_USER,
@@ -27,16 +27,12 @@ import {
   addPin,
   setLeaves
 } from '../store/actions'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import * as Location from 'expo-location'
 import TopNavigation from '../components/TopNavigation'
 
-import Modal, {
-  ModalTitle,
-  ModalContent,
-  ModalFooter,
-  ModalButton
-} from 'react-native-modals'
+import Modal, { ModalTitle, ModalContent, ModalFooter, ModalButton } from 'react-native-modals';
+
 
 
 export default function SnapScreen() {
@@ -55,12 +51,12 @@ export default function SnapScreen() {
   let camera = null
 
   const locationReducer = useSelector(state => state.locationReducer)
-  const {location} = locationReducer
+  const { location } = locationReducer
 
   useEffect(() => getLocation(), [])
   useEffect(() => {
     async function startUp() {
-      const {status} = await Permissions.askAsync(Permissions.CAMERA)
+      const { status } = await Permissions.askAsync(Permissions.CAMERA)
       setHasCameraPermission('granted')
       FileSystem.makeDirectoryAsync(FileSystem.cacheDirectory + 'photos').catch(
         e => {
@@ -73,7 +69,7 @@ export default function SnapScreen() {
 
   const fetchLocationAsync = async () => {
     try {
-      let {status} = await Permissions.askAsync(Permissions.LOCATION)
+      let { status } = await Permissions.askAsync(Permissions.LOCATION)
       if (status !== 'granted') {
         let errorMessage = 'Permission to access location was denied'
         console.log(errorMessage)
@@ -90,11 +86,11 @@ export default function SnapScreen() {
   }
   const takePicture = () => {
     if (camera) {
-      camera.takePictureAsync({onPictureSaved})
+      camera.takePictureAsync({ onPictureSaved })
     }
   }
 
-  const buttonCallback = function() {
+  const buttonCallback = function () {
     setIsPlantInfoReceived(false)
   }
 
@@ -115,12 +111,20 @@ export default function SnapScreen() {
       .post(`http://${ipAddressOfServer}:1234/image`, formData)
       .then(response => {
         console.log(response.data.commonName)
+<<<<<<< HEAD
         // alert(
         //   `Plant identified: ${response.data.commonName} \n probability: ${
         //     response.data.score
         //   }`
         // )
 
+=======
+        alert(
+          `Plant identified: ${response.data.commonName} \n probability: ${
+          response.data.score
+          }`
+        )
+>>>>>>> same commit as before but without husky plugin
         // if (response.data.score < 0.5) { throw(new Error) }
 
         /*
@@ -256,19 +260,19 @@ export default function SnapScreen() {
       <>
         <TopNavigation />
         <View style={styles.container}>
+
           <Modal
             visible={isWelcomeModalVisible}
             modalTitle={
-              <View style={{flexDirection: 'row'}}>
-                <ModalTitle
-                  title={
-                    <>
-                      <Ionicons name="ios-leaf" color="#6CC7BD" size={25} />
-                      <Text> Welcome to Plaize </Text>
-                      <Ionicons name="ios-leaf" color="#6CC7BD" size={25} />
-                    </>
-                  }
-                />
+              <View style={{ flexDirection: 'row' }}>
+                <ModalTitle  title=
+                {
+                <>
+                <Ionicons name="ios-leaf" color="#6CC7BD" size={25} />
+                <Text>       Welcome to Plaize       </Text>
+                <Ionicons name="ios-leaf" color="#6CC7BD" size={25} />
+                </>
+                } />
               </View>
             }
             width={0.7}
@@ -276,21 +280,19 @@ export default function SnapScreen() {
               <ModalFooter>
                 <ModalButton
                   text="OK"
-                  onPress={() => {
-                    setIsWelcomeModalVisible(false)
-                  }}
+                  onPress={() => { setIsWelcomeModalVisible(false) }}
                 />
               </ModalFooter>
             }
           >
             <ModalContent>
-              <Text> Let's jump right into it !</Text>
+              <Text>                                                                    Let's jump right into it !</Text>
             </ModalContent>
           </Modal>
         </View>
 
         {/* TOP 'NAVIGATION' */}
-        <View style={{flex: 0.07, flexDirection: 'row', marginTop: 15}}>
+        <View style={{ flex: 0.07, flexDirection: 'row', marginTop: 15 }}>
           <View
             style={{
               width: '33.3%',
@@ -369,12 +371,20 @@ export default function SnapScreen() {
         </View>
         {/* END TOP 'NAVIGATION' */}
 
+<<<<<<< HEAD
         <View style={{flex: 1, borderTopWidth: 0}}>
+=======
+        <View style={{ flex: 1 }}>
+>>>>>>> same commit as before but without husky plugin
           <Camera
             ref={ref => {
               camera = ref
             }}
+<<<<<<< HEAD
             style={{flex: 1, borderTopWidth: 0}}
+=======
+            style={{ flex: 1 }}
+>>>>>>> same commit as before but without husky plugin
             type={Camera.Constants.Type.back}
           >
             <View
@@ -395,7 +405,7 @@ export default function SnapScreen() {
                 <Ionicons
                   name="md-camera"
                   size={48}
-                  style={{marginBottom: 30}}
+                  style={{ marginBottom: 30 }}
                 />
               </TouchableOpacity>
             </View>
@@ -420,6 +430,7 @@ const Container = styled.View`
   align-self: center;
   width: 80%;
 `
+
 
 const styles = StyleSheet.create({
   container: {
