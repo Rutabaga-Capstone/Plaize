c6ede690786cfb43c0a0026d0a0fcdc56c9291ec
Pawel Bala <pawel3ala@gmail.com>
2019-12-06 12:14:47 -0600

    replacing the working seed file

--- server/seed.js
+++ server/seed.js
@@ -69,9 +69,6 @@ client
           email: "cc"
           password: "cc"
           leaves: 0
-          imageURL: "https://www.petguide.com/wp-content/uploads/2019/03/poison-ivy-dogs-668x444.jpg"
-          description: "A poisonous Asian and Eastern North American flowering plant in the genus Toxicodendron. Causes rashes when touched"
-          isPoisonous: true"
           regDate: {formatted: "2019-09-24"}
         ) {
           id
