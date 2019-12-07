dc3da45d11f6dc4d4d470c12305e115553e85d79
randy <randydxp@gmail.com>
2019-12-07 10:36:24 -0600

    Created seed1

--- client/navigation/AppNavigator.js
+++ client/navigation/AppNavigator.js
@@ -7,7 +7,7 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Login: HomeStack,
+    // Login: HomeStack,
     Main: MainTabNavigator
   })
 )
