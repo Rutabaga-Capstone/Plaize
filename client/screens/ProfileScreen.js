a4e92f8a84467dd911a288d5121d62e8c6d2355e
Sean <sean86753@gmail.com>
2019-12-08 11:56:33 -0600

    Update ProfileScreen.js
    Working with fakeView to account for margin

--- client/screens/ProfileScreen.js
+++ client/screens/ProfileScreen.js
@@ -10,10 +10,10 @@ import {
   AsyncStorage,
   TouchableHighlight
 } from 'react-native'
-import { withApollo } from 'react-apollo'
-import { connect } from 'react-redux'
-import { GET_USER_PROFILE_INFO } from '../constants/GqlQueries'
-import { Ionicons, SimpleLineIcons } from '@expo/vector-icons'
+import {withApollo} from 'react-apollo'
+import {connect} from 'react-redux'
+import {GET_USER_PROFILE_INFO} from '../constants/GqlQueries'
+import {Ionicons, SimpleLineIcons} from '@expo/vector-icons'
 import TopNavigation from '../components/TopNavigation'
 import RenderImages from './TempImageRender'
 
@@ -23,9 +23,9 @@ class ProfileScreen extends React.Component {
   }
 
   async componentDidMount() {
-    const { client } = this.props
+    const {client} = this.props
     try {
-      const { email } = JSON.parse(
+      const {email} = JSON.parse(
         (await AsyncStorage.getItem('LOGGED_IN_USER')) || '{}'
       )
       const result = await client.query({
@@ -34,7 +34,7 @@ class ProfileScreen extends React.Component {
           email
         }
       })
-      const { user } = result.data
+      const {user} = result.data
 
       // Quick fix for demo
       // TODO: to be fixed properly
@@ -47,7 +47,7 @@ class ProfileScreen extends React.Component {
         currentDate.getFullYear()
       user.regDate = regDate
 
-      this.setState({ user })
+      this.setState({user})
     } catch (err) {
       console.log(err)
     }
@@ -71,7 +71,7 @@ class ProfileScreen extends React.Component {
   }
 
   logoutUser = async () => {
-    const { navigate } = this.props.navigation
+    const {navigate} = this.props.navigation
     try {
       await AsyncStorage.removeItem('LOGGED_IN_USER')
       navigate('Home')
@@ -81,29 +81,27 @@ class ProfileScreen extends React.Component {
   }
 
   render() {
-    const { navigate } = this.props.navigation
-    const { userPlants } = this.props.plantsReducer
-    const { name, regDate, plants } = this.state.user
+    const {navigate} = this.props.navigation
+    const {userPlants} = this.props.plantsReducer
+    const {name, regDate, plants} = this.state.user
 
-    const { leaves } = this.props.leavesReducer
+    const {leaves} = this.props.leavesReducer
     return (
-      <>
-        <TopNavigation />
-
-        <View style={{ alignItems: 'center', alignSelf: 'stretch', flex: 1 }}>
-          <ScrollView contentContainerStyle={styles.contentContainer}>
-            {/* Welcome Container */}
-            <View style={styles.welcomeContainer}>
-
-              <Image
-                source={
-                  __DEV__
-                    ? require('../assets/images/profile-icon.png')
-                    : require('../assets/images/profile-icon.png')
-                }
-                style={styles.welcomeImage}
-              />
-              <Text style={styles.title}>{name}</Text>
+      <View style={{alignItems: 'center', alignSelf: 'stretch', flex: 1}}>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          {/* Welcome Container */}
+          <View style={styles.fakeView} />
+          <View style={styles.welcomeContainer}>
+            <TopNavigation />
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/profile-icon.png')
+                  : require('../assets/images/profile-icon.png')
+              }
+              style={styles.welcomeImage}
+            />
+            <Text style={styles.title}>{name}</Text>
             {/* Rank Level, Rank Number Container */}
             <View
               style={{
@@ -165,148 +163,85 @@ class ProfileScreen extends React.Component {
               }}
             />
 
-              {/* Rank Level, Rank Number Container */}
+            {/* PARENT Container View - Plants Text, Images, Map, etc */}
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'col',
+                justifyContent: 'space-between'
+              }}
+            >
+              {/* 'Poisonous Plants Identified:' TEXT Container */}
               <View
                 style={{
                   flex: 1,
-                  flexDirection: 'row'
+                  //flexDirection: 'col'
+                  marginBottom: 20
                 }}
               >
                 <Text
                   style={{
-                    width: '50%',
-                    height: 50,
-                    textAlign: 'right',
-                    fontSize: 24
-                  }}
-                >
-                  {this.getRankLevel(leaves)}
-                </Text>
-                <Text
-                  style={{
-                    width: '5%',
+                    width: '6%',
                     height: 50,
                     textAlign: 'center',
                     fontSize: 24,
                     color: '#C7CAD4'
                   }}
-                >
-                  •
-              </Text>
+                  //Blank Placeholder
+                />
                 <Text
                   style={{
-                    width: '5%',
+                    width: '100%',
                     height: 50,
-                    textAlign: 'center',
-                    fontSize: 24,
-                    color: '#C7CAD4'
+                    textAlign: 'left',
+                    fontSize: 20,
+                    color: '#000000',
+                    marginLeft: 25
                   }}
                 >
-                  <Ionicons name="ios-leaf" color="#6CC7BD" size={25} />
+                  Poisonous Plants Identified:
                 </Text>
                 <Text
                   style={{
-                    width: '40%',
+                    width: '34%',
                     height: 50,
                     textAlign: 'left',
                     fontSize: 24
                   }}
                 >
-                  {leaves}
+                  {/* Blank Placeholder */}
                 </Text>
               </View>
+              {/* End Row of 'Poisonous Plants Identified:' TEXT Container*/}
 
-              {/* Joined Plaze on JoinDate Row */}
-              <Text style={styles.subtitle}>
-                Joined Plaze on {regDate}
-              </Text>
-
-              <View
-                style={{
-                  flex: 1,
-                  marginTop: -40
-                }}
-              />
-
-              {/* PARENT Container View - Plants Text, Images, Map, etc */}
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: 'col',
-                  justifyContent: 'space-between'
-                }}
-              >
-                {/* 'Poisonous Plants Identified:' TEXT Container */}
+              {/* Start Row of Plant IMAGES */}
+              <View style={styles.plantsContainer}>
                 <View
                   style={{
                     flex: 1,
-                    //flexDirection: 'col'
-                    marginBottom: 20
+                    //flexDirection: 'col',
+                    flexWrap: 'wrap'
                   }}
                 >
-                  <Text
-                    style={{
-                      width: '6%',
-                      height: 50,
-                      textAlign: 'center',
-                      fontSize: 24,
-                      color: '#C7CAD4'
-                    }}
-                  //Blank Placeholder
-                  />
-                  <Text
-                    style={{
-                      width: '100%',
-                      height: 50,
-                      textAlign: 'left',
-                      fontSize: 20,
-                      color: '#000000',
-                      marginLeft: 25
-                    }}
-                  >
-                    Poisonous Plants Identified:
-                </Text>
-                  <Text
-                    style={{
-                      width: '34%',
-                      height: 50,
-                      textAlign: 'left',
-                      fontSize: 24
-                    }}
-                  >
-                    {/* Blank Placeholder */}
-                  </Text>
-                </View>
-                {/* End Row of 'Poisonous Plants Identified:' TEXT Container*/}
-
-                {/* Start Row of Plant IMAGES */}
-                <View style={styles.plantsContainer}>
                   <View
                     style={{
                       flex: 1,
-                      //flexDirection: 'col',
+                      flexDirection: 'row',
                       flexWrap: 'wrap'
                     }}
                   >
-                    <View
-                      style={{
-                        flex: 1,
-                        flexDirection: 'row',
-                        flexWrap: 'wrap'
-                      }}
-                    >
-                      <RenderImages {...this.props} />
-                    </View>
+                    <RenderImages {...this.props} />
                   </View>
                 </View>
-                {/* End Row of 'Poisonous Plants Identified' IMAGES*/}
               </View>
-              {/* Plaze Map Container */}
+              {/* End Row of 'Poisonous Plants Identified' IMAGES*/}
             </View>
-            {/* End Parent Container View */}
-          </ScrollView>
-        </View>
-      </>
+            <Text style={styles.plantText}>Poison Ivy</Text>
+            {/* Plaze Map Container */}
+          </View>
+          {/* End Parent Container View */}
+        </ScrollView>
+      </View>
     )
   }
 }
@@ -476,7 +411,7 @@ const styles = StyleSheet.create({
     ...Platform.select({
       ios: {
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
+        shadowOffset: {width: 0, height: -3},
         shadowOpacity: 0.1,
         shadowRadius: 3
       },
@@ -509,7 +444,7 @@ const styles = StyleSheet.create({
   }
 })
 
-export default connect(({ plantsReducer, leavesReducer }) => ({
+export default connect(({plantsReducer, leavesReducer}) => ({
   plantsReducer,
   leavesReducer
 }))(withApollo(ProfileScreen))
