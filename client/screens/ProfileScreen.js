6305354f2f999385089c27d89df4d87673713077
Pawel Bala <pawel3ala@gmail.com>
2019-12-08 19:39:44 -0600

    removing unused code in ProfileScreen

--- client/screens/ProfileScreen.js
+++ client/screens/ProfileScreen.js
@@ -250,40 +250,6 @@ ProfileScreen.navigationOptions = {
   header: null
 }
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    )
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    )
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    )
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  )
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  )
-}
-
 const styles = StyleSheet.create({
   plantText: {
     marginTop: 190,
