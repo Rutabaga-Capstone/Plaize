0336792d683f0f31cfbf293e42e158836a8f9672
Sean Duff <gatewaywebdesign18@gmail.com>
2020-01-02 13:39:39 -0600

    added external stylesheet for MapScreen, got rid of styles2 and styles99

--- client/screens/MapScreen.js
+++ client/screens/MapScreen.js
@@ -55,6 +55,7 @@ import Plants from '../components/Plants'
 import {Slider} from 'react-native-elements'
 import SwitchSelector from 'react-native-switch-selector'
 import {TagSelect} from 'react-native-tag-select'
+import styles from '../styles/MapScreenStyles'
 
 export default function MapScreen(props) {
   let sortedPins = []
@@ -291,91 +292,3 @@ const ButtonText = styled.Text`
   font-size: 20px;
   font-weight: 600;
 `
-
-const styles = {
-  container: {
-    width: '100%',
-    height: '100%'
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0
-  },
-  // mapContainer: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center'
-  // },
-  topNav: {
-    position: 'absolute',
-    top: 0,
-    zIndex: 10000000
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height / 2,
-    marginTop: 0
-  }
-}
-
-const styles2 = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    marginTop: 400,
-    marginLeft: 190,
-    position: 'absolute'
-  },
-  buttonContainer: {
-    padding: 5
-  },
-  buttonInner: {
-    marginBottom: 10
-  },
-  labelText: {
-    color: '#333',
-    fontSize: 12,
-    fontWeight: '500',
-    marginBottom: 15
-  },
-  item: {
-    borderWidth: 1,
-    borderColor: '#333',
-    backgroundColor: '#FFF'
-  },
-  label: {
-    color: '#333'
-  },
-  itemSelected: {
-    backgroundColor: '#333'
-  },
-  labelSelected: {
-    color: '#FFF'
-  }
-})
-
-const styles99 = StyleSheet.create({
-  activityIndicatorContainer: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
-  },
-
-  row: {
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-    padding: 10
-  },
-
-  title: {
-    fontSize: 15,
-    fontWeight: '600'
-  },
-
-  description: {
-    marginTop: 5,
-    fontSize: 14
-  }
-})
